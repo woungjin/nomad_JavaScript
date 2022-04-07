21a24f624894adcdbda85a9b3b18965fc83befea
woungjin <jinwung3198@gamil.com>
2022-04-07 12:12:07 +0900

    객체생성 방법

---
+++ javascript/03(객체생성 방법).js
@@ -0,0 +1,12 @@
+// ==== ＊ 3.객체(Object) 생성방법 ====
+// ● 객체 == const object = { : , : , }
+const player = {
+    name: "jin",
+    points : 10,
+    fat : true,
+};
+console.log(player.name)
+console.log(player["name"])
+player.fat = false;         // 객체의 참조로는 const의 값을 변경 가능 
+player.lastName = "potato"; // 새 프로퍼티 생성가능
+console.log(player)
