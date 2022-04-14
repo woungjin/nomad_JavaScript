9b317e68768348822d5f0686b08dd31103b2176b
woungjin <jinwung3198@gamil.com>
2022-04-14 12:36:48 +0900

    createElement() & appendChild() 활용한 배경이미지 입히기

--- javascript/js/11background.js
+++ javascript/js/11background.js
@@ -4,14 +4,12 @@
 // : Element생성시 사용
 // ● appendChild()
 // : 해당 Element를 body에 적용시킬때 사용
-const images = ["0.jpg","1.jpg","2,jpg"];
+const images = ["0.jpg","1.jpg","2,jpg"]; // 1. 이미지 배열 생성 
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)]; // 2. 이미지의 랜덤한 값을 가져옴
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img"); // 3. img태그 생성 
 
-console.log(bgImage)
+bgImage.src = `img/${chosenImage}`; // 4. img태그에 src경로를 입력 
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage); // prepend() : 맨처음에 더함 / append : 맨마지막에 더함
+document.body.appendChild(bgImage); // 5. body에 img태그추가 prepend() : 맨처음에 더함 / append : 맨마지막에 더함
