438c5cc48de32a425c1935967aa636a3ab78dfe8
woungjin <jinwung3198@gamil.com>
2022-04-14 13:06:41 +0900

    createElement() / appendChild() / append()의 차이점과 배경이미지 생성하기

--- javascript/js/11background.js
+++ javascript/js/11background.js
@@ -6,7 +6,7 @@
 // : 해당 Element를 body에 적용시킬때 사용 객체만을 저장할 수 있음 
 // ● append()
 // : 문자열등 객체 추가가능 appendChild()보다 많은것을 저장함  
-const images = ["0.jpg","1.jpg","2,jpg"]; // 1. 이미지 배열 생성 
+const images = ["0.jpg","1.jpg","2.jpg"]; // 1. 이미지 배열 생성 
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]; // 2. 이미지의 랜덤한 값을 가져옴
 
--- javascript/js/12todo.js
+++ javascript/js/12todo.js
@@ -6,6 +6,15 @@ function handleToDoSubmit(event) { // 5. form의  submit 함수를 실행
     event.preventDefault(); // 6. submit 중단
     const newTodo = toDoInput.value; // 7. value값 저장
     toDoInput.value = ""; // 8. 값 전달시 다시 input내용을 비움
+    paintToDo(newTodo);
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit); // 4. form의 submit event를 추가함
